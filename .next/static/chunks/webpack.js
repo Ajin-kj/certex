263c3d4954693b680e8c77de2e64be85cf98237b
Ajin-kj <ajin.kj@experionglobal.com>
2025-06-04 13:03:17 +0530

    Updated trace

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 		// This function allow to reference async chunks
 /******/ 		__webpack_require__.u = (chunkId) => {
 /******/ 			// return url for filenames based on template
-/******/ 			return undefined;
+/******/ 			return "static/chunks/" + chunkId + ".js";
 /******/ 		};
 /******/ 	})();
 /******/ 	
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("b66a95c7210881ad")
+/******/ 		__webpack_require__.h = () => ("cb77ce6672c42ef7")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
